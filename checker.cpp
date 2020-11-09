4018699832797a3cf56b1afedea015c1b09bcd40
himanshu007-creator <65963997+himanshu007-creator@users.noreply.github.com>
2020-11-09 23:15:43 +0530

    Create checker.cpp

---
+++ checker.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char input[1000];
+int Stack[1000], ind;
+
+void push (int x) {
+    Stack[++ind] = x;
+}
+
+bool isEmpty() {
+    if (ind > 0) return false;
+            else return true;
+}
+
+bool verify (char input[]) {
+    ind = 0;
+    int n = strlen(input);
+
+    for (int i=0; i<n; ++i) {
+        if (input[i] == '(') push(1);
+        if (input[i] == '[') push(2);
+        if (input[i] == '{') push(3);
+
+        if (input[i] == ')') {
+           
+            if (isEmpty() || Stack[ind] != 1) return false;
+            else {
+                Stack[ind] = 0;
+                --ind;
+            }
+        }
+        if (input[i] == ']') {
+           
+            if (isEmpty() || Stack[ind] != 2) return false;
+            else {
+                Stack[ind] = 0;
+                --ind;
+            }
+        }
+        if (input[i] == '}') {
+     
+            if (isEmpty() || Stack[ind] != 3) return false;
+            else {
+                Stack[ind] = 0;
+                --ind;
+            }
+        }
+    }
+
+    if (ind == 0) return true; // if the stack is empty, then the input is OK
+             else return false;
+}
+
+int main()
+{
+    cin>>input;
+
+    cout<<verify (input);
+
+    return 0;
+}
